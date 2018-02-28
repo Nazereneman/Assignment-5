201cfb699526cb912b7f853690c5037c09b9c3ee
Nazereneman <36425688+Nazereneman@users.noreply.github.com>
2018-02-28 12:12:58 -0600

    while loop to automatically check number of pts required

--- MA_Assign_5A.c
+++ MA_Assign_5A.c
@@ -14,13 +14,15 @@ calculations?
 int main() {
     double i ;                                              //declare i
     double x,y ;                                            //declare x and y
-    double N=1000 ;                                         //declare N (how many points there are)
+    double N=1 ;                                            //declare N (how many points there are)
     double C=0,S=0;                                         //declare C & S (# of points in circle/square)
     double Pi ;                                             //declare Pi (approximate value for pi from Monte Carlo)
-    double Ea,Er ;                                          //declare Error (difference between pi and approx)
+    double Ea=1,Er ;                                        //declare Error (difference between pi and approx)
     double PI=4.*atan(1.) ;                                 //Declare pi
+    double var=0 ;                                          //Declare variable to check for first time correct decimal places
 //Computations
-    for (i=1;i<N;i++) {                                     //for loop to generate N points for copmutation
+while (Ea>=pow(10,-16)){
+    for (i=0;i<N;i++) {                                     //for loop to generate N points for copmutation
         x=(double)rand()/(double)RAND_MAX ;                 //Generate random numbers between 0 to 1
         y=(double)rand()/(double)RAND_MAX ;                 //Generate random numbers between 0 to 1
         if (pow(x,2)+pow(y,2)<=1) {
@@ -29,11 +31,27 @@ int main() {
     S=S+1 ;                                                 //Increase S by one for each point (all points will be in square)
     }
     Pi=4*C/S ;                                              //Approximate Pi from Monte Carlo
-    Ea=fabs(PI-Pi) ;                                         //Calculate abs error in Pi
-    Er=fabs(PI-Pi)/PI ;                                      //Calculate relative error in Pi
-
+    Ea=fabs(PI-Pi) ;                                        //Calculate abs error in Pi
+    Er=fabs(PI-Pi)/PI ;                                     //Calculate relative error in Pi
+    if (Ea<=pow(10,-3) & var<=0){                                       //Condition to get 2 decimal points of accuracy
+    printf("\nNumber of points to get 2 decimal places is %.0lf",N);    //Print the Number of points to get 2 decimal points
+    var=var+1 ;                                                         //Increase variable as to only show the first time 2 decimal points of accuracy
+    }
+    else if (Ea <= pow(10,-5) & var<=1){                                //Condition to get 4 decimal points of accuracy
+    printf("\nNumber of points to get 4 decimal places is %.0lf",N);    //Print the Number of points to get 4 decimal points
+    var=var+1 ;                                                         //Increase variable as to only show the first time 4 decimal points of accuracy
+    }
+    else if (Ea <= pow(10,-9) & var<=2) {                               //Condtiion to get 8 decimal points of accuracy
+    printf("\nNumber of points to get 8 decimal places is %.0lf",N);    //Print the number of points to get 8 decimal points
+    var=var+1 ;                                                         //Increase variable as to only show the first time 8 decimal points of accuracy
+    }
+    else if (Ea <= pow(10,-17) & var<=3){                               //Condtion to get 16 decimal points of accuracy
+    printf("\nNumber of points to get 16 decimal places is %.0lf",N);   //Print the number of points to get 16 decimal points
+    var=var+1 ;                                                         //Increase variable as to only show the first time 16 decimal points of accuracy
+    }
+N=N+1 ;                                                     //Increase N by one every loop
+}
 //Print Values
-    printf("\nNumber of points used is %.0lf",N) ;            //Print the number of points used
     printf("\nApproximate value for Pi is %.8lf",Pi) ;      //Print the approximate value of Pi
     printf("\nAbsolute error is %.16lf",Ea) ;               //Print the absolute error
     printf("\nRelative error is %.16lf",Er) ;               //Print the relative error
@@ -41,3 +59,6 @@ int main() {
     
 return 0;
 }
+
+
+
